9fec2eb7948a486f6348b5748ab6a5e84c118be1
TigerX <100389933+TigerX1112@users.noreply.github.com>
2023-01-08 19:46:34 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 song1 = "";
 song2 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
 function preload() {
     song1 = loadSound("sorry.mp3");
     song2 = loadSound("as_it_was.mp3");
@@ -11,7 +17,26 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modalLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function modalLoaded() {
+    console.log("Pose net is intilized");
 }
 function draw() {
     image(video, 0, 0, 450, 400);
+}
+function gotPoses(results) {
+    if(results.length > 0){
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("X = " + leftWristX + " Y = " + leftWristY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("X = " + rightWristX + " y = " + rightWristY);
+    }
 }
